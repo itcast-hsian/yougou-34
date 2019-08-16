bd5b6a408b4e7c835f66f5dc09adb0a8620bea33
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-08-16 11:01:51 +0800

    首页轮播图

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+import request from "./utils/request.js";
+
 //app.js
 App({
+  //项目运行时候触发，只会执行一次
   onLaunch: function () {
+    // 初始化reqeust基准路径
+    request.defaults.baseURL = "https://api.zbztb.cn/api/public/v1";
 
+    // 错误拦截
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,28 @@
+import request from "../../utils/request.js";
 
 Page({
   
+  data: {
+    autoplay: true,
+    // 轮播图的数据
+    imgUrls: [
+      // 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      // 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      // 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ]
+  },
+
+  onLoad(){
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      // 返回的数组
+      const {message} = res.data;
+      // 修改imgUrls
+      this.setData({
+        imgUrls: message
+      })
+    })
+  }
+
 })
