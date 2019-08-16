1330f5c053e39cfc97182fe13d95cc474e5c91d8
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-08-16 11:53:56 +0800

    菜单和楼层渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,10 +9,15 @@ Page({
       // 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
       // 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       // 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-    ]
+    ],
+    // 菜单数据
+    menus: [],
+    // 楼层数据
+    floors: []
   },
 
   onLoad(){
+    // 请求轮播图数据
     request({
       url: "/home/swiperdata"
     }).then(res => {
@@ -21,6 +26,28 @@ Page({
       // 修改imgUrls
       this.setData({
         imgUrls: message
+      })
+    });
+
+    // 请求菜单的数据
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      const {message} = res.data;
+
+      this.setData({
+        menus: message
+      })
+    })
+
+    // 请求楼层数据
+    request({
+      url: "/home/floordata"
+    }).then(res => {
+      const {message} = res.data;
+
+      this.setData({
+        floors: message
       })
     })
   }
