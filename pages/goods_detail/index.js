915b0bc9957463c61549f1c3b38d1caff20df00f
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-08-17 16:23:59 +0800

    请求商品详情的接口

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,4 +1,6 @@
-// pages/goods_details/index.js
+
+import request from "../../utils/request.js"
+
 Page({
 
   /**
@@ -10,12 +12,29 @@ Page({
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
+
+    // 商品详情
+    detail: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const {id} = options;
 
+    // 请求商品详情
+    request({
+      url: "/goods/detail",
+      data: {
+        goods_id: id
+      }
+    }).then(res => {
+      const {message} = res.data;
+
+      this.setData({
+        detail: message
+      })
+    })
   },
 })
