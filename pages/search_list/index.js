150b3fb4689b4a26454d36f11068cd6b88eae1e5
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-08-17 11:40:58 +0800

    分类页跳转到搜索页

--- pages/search_list/index.js
+++ pages/search_list/index.js
@@ -1,11 +1,18 @@
-// pages/search_list/index.js
+
+import request from "../../utils/request.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    current: 0
+    current: 0,
+
+    keyword: "",
+
+    // 商品列表
+    goods: [],
   },
 
   handleChange(event){
@@ -21,5 +28,29 @@ Page({
    */
   onLoad: function (options) {
 
+
+    // 假设关键字 小米
+    // let keyword = "小米";
+
+    // options返回url的参数，类似this.$router.query
+    this.setData({
+      keyword: options.keyword
+    })
+
+    request({
+      url: "/goods/search?query=" + options.keyword,
+    }).then(res => {
+      const { goods } = res.data.message;
+
+      // 循环给每个商品修改价格，保留两位小数点
+      const newGoods = goods.map(v => {
+        v.goods_price = Number(v.goods_price).toFixed(2);
+        return v;
+      })
+
+      this.setData({
+        goods: newGoods
+      })
+    })
   },
 })
