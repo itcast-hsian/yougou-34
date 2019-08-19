86f66c899618ca4eda9920868ccb61acba05895e
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-08-19 10:22:11 +0800

    搜索取消

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    inputValue: "", // 输入框的值
   },
 
   /**
@@ -15,52 +15,19 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 没次输入搜索关键字时候都会触发
+  handleInput(event){
+    const {value} = event.detail;
 
+    this.setData({
+      inputValue: value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 清空输入框的值
+  handleCancel(){
+    this.setData({
+      inputValue:""
+    })
   }
 })
